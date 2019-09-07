6958069cc32cf4c3ba860766ca5cd3fa3a8321a2
DanielAlonsoTM <danielinogordo@gmail.com>
2019-09-07 01:10:15 -0400

    Reformat snackBarMessage function

--- app/src/main/java/com/glados/gladosapp/utils/GraphicsUtilities.kt
+++ app/src/main/java/com/glados/gladosapp/utils/GraphicsUtilities.kt
@@ -7,18 +7,18 @@ import android.view.WindowManager
 import androidx.core.content.ContextCompat
 import com.glados.gladosapp.R
 
-fun changeFullColorAppBar(context: Context, window: Window, actBar: ActionBar?) {
+object GraphicsUtilities {
+    fun changeFullColorAppBar(context: Context, window: Window, actBar: ActionBar?) {
 
-    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-    window.statusBarColor = ContextCompat.getColor(context, R.color.colorPrimary)
-    window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(context, R.color.colorPrimary)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
 //    val actionBar: ActionBar? = actBar
 //    actionBar?.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.gradient_actionbar))
 //    actionBar?.setBackgroundDrawable(resources.getDrawable(R.drawable.gradient_actionbar))
-    actBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
-    actBar?.setCustomView(R.layout.actionbar_custom)
-
-
+        actBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        actBar?.setCustomView(R.layout.actionbar_custom)
+    }
 }
