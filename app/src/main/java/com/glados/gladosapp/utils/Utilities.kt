52e8c26971d58a4e43071580bf2146823da642d8
DanielAlonsoTM <danielinogordo@gmail.com>
2019-09-07 01:11:39 -0400

    Graphic resource

--- app/src/main/java/com/glados/gladosapp/utils/Utilities.kt
+++ app/src/main/java/com/glados/gladosapp/utils/Utilities.kt
@@ -3,7 +3,9 @@ package com.glados.gladosapp.utils
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
-fun snackBarMessage(view: View, message: String) {
-    Snackbar.make(view, message, Snackbar.LENGTH_LONG)
-        .setAction("Action", null).show()
+object Utilities {
+    fun snackBarMessage(view: View, message: String) {
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
+    }
 }
