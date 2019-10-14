95bd2975aa5f2e58354d3720c92c899328d463c4
DanielAlonsoTM <danielinogordo@gmail.com>
2019-10-14 18:43:31 -0300

    Creation of alert dialogs for MainActivity

---
+++ app/src/main/java/com/glados/gladosapp/utils/WifiUtilities.kt
@@ -0,0 +1,26 @@
+package com.glados.gladosapp.utils
+
+import android.content.Context
+import android.net.wifi.SupplicantState
+import android.net.wifi.WifiManager
+import android.view.View
+import com.glados.gladosapp.utils.Utilities.snackBarMessage
+
+object WifiUtilities {
+
+    fun netWifiService(context: Context, view: View) {
+
+
+        val wifiManager = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val wifiInfo = wifiManager.connectionInfo
+
+        if (wifiInfo.supplicantState == SupplicantState.COMPLETED) {
+            val ssid = wifiInfo.ssid
+            print("BSSID: $ssid")
+            snackBarMessage(view, ssid)
+        } else {
+            print("No hay fiutos")
+            snackBarMessage(view, "Cueck")
+        }
+    }
+}
