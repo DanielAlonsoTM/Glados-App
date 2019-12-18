73e55a23a173c89136baf95e3a97fe5ffa08002d
DanielAlonsoTM <DreamRider2012>
2019-12-18 03:37:56 -0300

    Client service is built for communication with the server

---
+++ app/src/main/java/com/glados/gladosapp/communication/ClientService.kt
@@ -0,0 +1,32 @@
+package com.glados.gladosapp.communication
+
+import org.zeromq.ZMQ
+
+object ClientService {
+    fun sendMessageToServer(message: String) {
+        val context = ZMQ.context(1)
+        try {
+
+            //Set socket configuration
+            val socket = context.socket(ZMQ.REQ)
+            socket.connect("tcp://192.168.1.88:5897")
+
+            val request = "$message "
+
+            //Convert request to ByteArray
+            val byteRequest = request.toByteArray()
+            byteRequest[byteRequest.size - 1] = 0
+
+            //Send a request to server
+            socket.send(byteRequest, 0)
+
+            //Receive response from server
+            val byteReply = socket.recv(0)
+            val plainReply = String(byteReply, 0, byteReply.size - 1)
+
+            println(plainReply)
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+        }
+    }
+}
--- app/src/main/java/com/glados/gladosapp/utils/DialogUtils.kt
+++ app/src/main/java/com/glados/gladosapp/utils/DialogUtils.kt
@@ -1,25 +1,31 @@
 package com.glados.gladosapp.utils
 
 import android.content.Context
+import android.content.DialogInterface
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatEditText
 import com.glados.gladosapp.R
+import com.glados.gladosapp.communication.ClientService
 
 object DialogUtils {
 
     fun alertDialog(view: View, context: Context, inflater: LayoutInflater) {
-        val builderDialog = AlertDialog.Builder(context)
-
         val dialogLayout = inflater.inflate(R.layout.popup_wifi, null)
-        val editText = dialogLayout.findViewById<AppCompatEditText>(R.id.passwordWifi)
-        val buttonAlert = dialogLayout.findViewById<Button>(R.id.buttonConfirmPassword)
 
-        buttonAlert.setOnClickListener { Utilities.snackBarMessage(view, editText.text.toString()) }
+        val builderDialog = AlertDialog.Builder(ContextThemeWrapper(context, R.style.AlertDialogCustom))
+        builderDialog.setTitle("Wifi Settings")
+
+        val editText = dialogLayout.findViewById<AppCompatEditText>(R.id.passwordWifi)
 
         builderDialog.setView(dialogLayout)
+        builderDialog.setPositiveButton("Ok") { _: DialogInterface?, _: Int ->
+            Thread(Runnable {
+                ClientService.sendMessageToServer(editText.text.toString())
+            }).start()
+        }
         builderDialog.show()
     }
 }
