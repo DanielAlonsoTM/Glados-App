995fc987cd6fa097b5282acdc169986352f5bbf1
DanielAlonsoTM <danielinogordo@gmail.com>
2020-02-18 02:16:06 -0300

    New icons: Email and Lock

--- app/src/main/java/com/samuraitech/gladosapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/samuraitech/gladosapp/ui/login/LoginActivity.kt
@@ -1,13 +1,25 @@
 package com.samuraitech.gladosapp.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 import com.samuraitech.gladosapp.R
+import com.samuraitech.gladosapp.ui.main.MainActivity
+import com.samuraitech.gladosapp.utils.GraphicsUtilities.makeTransparentActionBar
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        makeTransparentActionBar(window, supportActionBar)
+
+        val buttonSignUp = findViewById<AppCompatButton>(R.id.btn_signup)
+
+        buttonSignUp.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/samuraitech/gladosapp/utils/GraphicsUtilities.kt
+++ app/src/main/java/com/samuraitech/gladosapp/utils/GraphicsUtilities.kt
@@ -2,6 +2,8 @@ package com.samuraitech.gladosapp.utils
 
 import androidx.appcompat.app.ActionBar
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.Window
 import android.view.WindowManager
 import androidx.core.content.ContextCompat
@@ -9,16 +11,19 @@ import com.samuraitech.gladosapp.R
 
 object GraphicsUtilities {
     fun changeFullColorAppBar(context: Context, window: Window, actBar: ActionBar?) {
-
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.statusBarColor = ContextCompat.getColor(context, R.color.colorPrimary)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-//    val actionBar: ActionBar? = actBar
-//    actionBar?.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.gradient_actionbar))
-//    actionBar?.setBackgroundDrawable(resources.getDrawable(R.drawable.gradient_actionbar))
+        actBar?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         actBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         actBar?.setCustomView(R.layout.actionbar_custom)
+    }
+
+    fun makeTransparentActionBar(window: Window, actBar: ActionBar?) {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        actBar?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
     }
 }
