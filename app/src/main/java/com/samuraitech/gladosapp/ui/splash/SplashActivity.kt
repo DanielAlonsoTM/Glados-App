64a398198281dff95243efaf97b8b1d3cd31a4a3
DanielAlonsoTM <danielinogordo@gmail.com>
2020-02-18 06:02:02 -0300

    Animations added

--- app/src/main/java/com/samuraitech/gladosapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/samuraitech/gladosapp/ui/splash/SplashActivity.kt
@@ -1,25 +1,40 @@
 package com.samuraitech.gladosapp.ui.splash
 
+import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.util.Pair
 import com.samuraitech.gladosapp.R
 import com.samuraitech.gladosapp.ui.login.LoginActivity
-import com.samuraitech.gladosapp.utils.GraphicsUtilities.makeTransparentActionBar
 
 class SplashActivity : AppCompatActivity() {
 
-    private val splashTimeOut: Long = 2000 // 2 sec
+    private val splashTimeOut: Long = 3000 // 3 sec
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-//        makeTransparentActionBar(window, supportActionBar)
+//        val iconSplash = findViewById<ImageView>(R.id.splash_icon)
+//        val titleSplash = findViewById<TextView>(R.id.splash_title)
+
+//        val pairs = ArrayList<Pair<View, String>>()
+//        pairs.add(Pair(iconSplash, iconSplash.transitionName))
+//        pairs.add(Pair(titleSplash, titleSplash.transitionName))
+
+//        val options = ActivityOptions.makeSceneTransitionAnimation(this, pairs[0], pairs[1])
+
+        val intent = Intent(this, LoginActivity::class.java)
 
         Handler().postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
+//            startActivity(intent, options.toBundle())
+            startActivity(intent)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
             finish()
         }, splashTimeOut)
     }
