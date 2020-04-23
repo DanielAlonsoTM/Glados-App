849ea8e0864fde532d7776b5d5988e9009d3b751
DanielAlonsoTM <danielinogordo@gmail.com>
2020-04-23 18:20:52 -0400

    New models

---
+++ app/src/main/java/com/samuraitech/gladosapp/model/Device.kt
@@ -0,0 +1,7 @@
+package com.samuraitech.gladosapp.model
+
+data class Device(
+    val id: Int,
+    val name: String,
+    val type: String
+)
--- app/src/main/java/com/samuraitech/gladosapp/model/Room.kt
+++ app/src/main/java/com/samuraitech/gladosapp/model/Room.kt
@@ -1,10 +1,12 @@
 package com.samuraitech.gladosapp.model
 
 data class Room(
+    val id: Int,
     val name: String,
     val lastActivity: String,
     val temperature: Int,
     val kilowattsConsumed: Int,
-    val humidity: Int
+    val humidity: Int,
+    val devicesInRoom: ArrayList<Device>
 )
 
